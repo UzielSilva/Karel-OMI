62d30beb54e42496fc016e9bb77e366b2f040bfa
german77 <juangerman-13@hotmail.com>
2014-02-05 20:43:20 -0600

    Help documentation

--- src/main/Views/Ventana2.java
+++ src/main/Views/Ventana2.java
@@ -1,5 +1,6 @@
 package main.Views;
 
+import java.io.IOException;
 import javax.swing.*;
 
 /**
@@ -9,10 +10,16 @@ public class Ventana2 extends JTabbedPane{
     public static TabMundo mundo=new TabMundo();
     public static TabPrograma programa=new TabPrograma();
     public static PopUp popup=new PopUp();
+    private JEditorPane help= new JEditorPane("text/html",""); 
     public Ventana2(){
         this.setFocusable(false);
         this.addTab("Mundo", mundo);
         this.addTab("Programa", programa);
         this.addTab("Ejecutar", new TabEjecutar());
+        this.addTab("Ayuda", new JScrollPane(help));
+        help.setEditable(false);
+        try {
+            help.setPage(getClass().getResource("HelpDoc/KarelSyntax_es.html"));
+        } catch (IOException ex) {}
     }
 }
